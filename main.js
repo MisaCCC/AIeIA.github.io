6c8c634554ea2e02f6edb575ed19e946f2330c6d
MisaCCC <78465311+MisaCCC@users.noreply.github.com>
2022-06-10 17:35:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('#temario.lista a').forEach((elemento) => {
 	elemento.addEventListener('click', () => {
 		document.querySelector('#temario.activo').classList.remove('activo');
-		elemento.parentElement.classList.add('activo')
-	})
+		elemento.parentElement.classList.add('activo');
+	});
