3c9fe5d53d350202bdd47c34cde0be4de8b97ad1
MisaCCC <78465311+MisaCCC@users.noreply.github.com>
2022-06-10 16:49:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('#temario .lista a').forEach((elemento) => {
+	elemento.addEventListener('click', () => {
+		document.querySelector('#temario .activo').classList.remove('activo');
+		elemento.parentElement.classList.add('activo');
+	});
